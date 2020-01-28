5512bac07aaeeb94f7ea1027bd869d4d4372c5aa
CasacoVermelho <alexandrebmjr@gmail.com>
2020-01-27 23:29:09 -0300

    Revert ":bug: teste mimetypes"
    This reverts commit 251a072a1c59ad62d0c1a29229db2e68e20a19f8.

--- tiresias/settings.py
+++ tiresias/settings.py
@@ -128,8 +128,8 @@ mimetypes.add_type("text/css", ".css", True)
 
 MEDIA_URL = '/images/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
